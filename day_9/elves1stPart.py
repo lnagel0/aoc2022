84ac4567ceea64f69575265a66878a08ff1f996d
Lucas <altingdesigns88@gmail.com>
2022-12-13 16:27:49 -0300

    1st part done.

--- day_9/elves1stPart.py
+++ day_9/elves1stPart.py
@@ -1,57 +1,35 @@
+import math
+
 input = open("input.txt", encoding="utf-8")
 
 instructions = []
-grid = []
-currPos = [4, 0]
-
-for y in range(5): # a 6x5 grid is built here
-    grid.append([])
-    for x in range(6):
-        grid[y].append('.')
+hx, hy, tx, ty = 0, 0, 0, 0
+dx, dy = 0, 0
+visited = set()
 
 for line in input:
     instructions.append(line.rstrip())
 
-def mvRight(currY, currX):
-    grid[currY][currX + 1] = 'H'
-    grid[currY][currX] = '.'
-    currPos = [currY, currX + 1]
-    return currPos
+for line in instructions:
+    dir, amt = line.split(' ')
 
-def mvLeft(currY, currX):
-    grid[currY][currX - 1] = 'H'
-    grid[currY][currX] = '.'
-    currPos = [currY, currX - 1]
-    return currPos
+    for i in range(int(amt)):
+        if dir == 'R':
+            hx += 1
+        elif dir == 'L':
+            hx -= 1
+        elif dir == 'U':
+            hy -= 1
+        elif dir == 'D':
+            hy += 1
+    
+        dx, dy = hx - tx, hy - ty
+        if abs(dx) > 1 or abs(dy) > 1:
+            if dx:
+                tx += math.copysign(1, dx)
+            if dy:
+                ty += math.copysign(1, dy)
 
-def mvUp(currY, currX):
-    grid[currY - 1][currX] = 'H'
-    grid[currY][currX] = '.'
-    currPos = [currY - 1, currX]
-    return currPos
+            visited.add((tx, ty))    
 
-def mvDown(currY, currX):
-    grid[currY + 1][currX] = 'H'
-    grid[currY][currX] = '.'
-    currPos = [currY + 1, currX]
-    return currPos
-
-for instruction in instructions:
-    input = instruction.split()
-    dir, amt = input
-
-    if dir == 'R':
-        for i in range(int(amt)):
-            currPos = mvRight(currPos[0], currPos[1])
-    elif dir == 'L':
-        for i in range(int(amt)):
-            currPos = mvLeft(currPos[0], currPos[1])
-    elif dir == 'U':
-        for i in range(int(amt)):
-            currPos = mvUp(currPos[0], currPos[1])
-    elif dir == 'D':
-        for i in range(int(amt)):
-            currPos = mvDown(currPos[0], currPos[1])
-
-for x in range(5): # grid
-    print(grid[x])
+print(len(visited))
